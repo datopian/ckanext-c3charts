bf40d4724250da99eb99e7db57465e09d885e089
Dusko Bogdanovski <dusko.bogdanovski@keitaro.com>
2016-10-31 19:09:42 +0100

    Improve color scheme

--- ckanext/c3charts/fanstatic/colors_scheme.js
+++ ckanext/c3charts/fanstatic/colors_scheme.js
@@ -5,15 +5,15 @@ $(document).ready(function () {
         var hex = '#' + $('#chart_base_color').val();
 
         var base = new KolorWheel(hex);
-        var target = base.abs('#ede7f2', 9);
+        var target = base.abs('#141314', 12);
         var color_scheme = [];
 
         target.each(function() {
             color_scheme.push(this.getHex());
         });
 
+       console.log(color_scheme)
        $('#color_scheme').val(color_scheme.toString());
 
     });
 });
-
