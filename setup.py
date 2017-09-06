15ed57b6ca11d5e2a4446e3452ef705452b02329
Petar Efnushev <petar.efnushev@keitaro.com>
2017-09-06 10:35:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages  # Always prefer setuptools over distutils
-from codecs import open  # To use a consistent encoding
-from os import path
 import os
+from os import path
+from setuptools import setup, find_packages
+from codecs import open
 
 here = path.abspath(path.dirname(__file__))
+
 
 # Recurse into package files
 def package_files(directory):
@@ -13,6 +14,7 @@ def package_files(directory):
         for filename in filenames:
             paths.append(os.path.join('..', path, filename))
     return paths
+
 
 # Get the long description from the relevant file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
@@ -34,11 +36,11 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/dumyan/ckanext-charts',
+    url='https://github.com/viderumglobal/ckanext-c3charts',
 
     # Author details
-    author='''Damjan Velickovski''',
-    author_email='''damjan.velickovski@keitaro.info''',
+    author='''Keitaro INC''',
+    author_email='''info@keitaro.com''',
 
     # Choose your license
     license='AGPL',
@@ -60,13 +62,13 @@ setup(
         'Programming Language :: Python :: 2.7',
     ],
 
-
     # What does your project relate to?
     keywords='''CKAN charts c3js d3js''',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    namespace_packages=['ckanext'],
 
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed. For an analysis of "install_requires" vs pip's
@@ -75,8 +77,8 @@ setup(
     install_requires=[],
 
     # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
+    # installed, specify them here.  If using Python 2.6 or less,
+    # then these have to be included in MANIFEST.in as well.
     include_package_data=True,
     package_data={
         '': css_files + image_files + html_files
@@ -94,8 +96,8 @@ setup(
     entry_points='''
         [ckan.plugins]
         c3charts=ckanext.c3charts.plugin:ChartsPlugin
-	[babel.extractors]
-	ckan = ckan.lib.extract:extract_ckan
+    [babel.extractors]
+    ckan = ckan.lib.extract:extract_ckan
     ''',
 
     # If you are changing from the default layout of your extension, you may
